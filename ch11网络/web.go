5d35d0b89fa956a35a2a085a72556954627b553f
小粽子 <862275679@qq.com>
2018-07-10 09:47:49 +0800

    web

---
+++ ch11网络/web.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	remPartOfURL := r.URL.Path[len("/hello/"):]
+	fmt.Fprintf(w, "Hello %s!", remPartOfURL)
+}
+
+func shoutHelloHandler(w http.ResponseWriter, r *http.Request) {
+	remPartOfURL := r.URL.Path[len("/shoutHello/"):]
+	fmt.Fprintf(w, "Hello %s!", strings.ToUpper(remPartOfURL))
+}
+
+func main() {
+	http.HandleFunc("/hello/", helloHandler)
+	http.HandleFunc("/shoutHello/", shoutHelloHandler)
+	http.ListenAndServe("localhost:9999", nil)
+}
