6aba04b48abd9644fa06a17643fff34152491abc
小粽子 <862275679@qq.com>
2018-08-08 11:43:51 +0800

    2018-08-08 11:43:51

--- ch12数据库/db.go
+++ ch12数据库/db.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
 	"log"
+	"fmt"
 )
 
 var (
@@ -13,25 +14,25 @@ var (
 
 func main() {
 	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3307)/mac")
-
 	rows, err := db.Query("select id, name from user where id > ?", 0)
-
-	if err != nil {
-		log.Fatal(err)
-	}
+	checkErr(err)
 
 	// 延迟释放
 	defer db.Close()
-
 	for rows.Next() {
 		err := rows.Scan(&id, &name)
-		if err != nil {
-			log.Fatal(err)
-		}
+		checkErr(err)
+
 		log.Println(id, name)
+		fmt.Println(id, name)
 	}
 	err = rows.Err()
+	checkErr(err)
+}
+
+func checkErr(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
+
 }
